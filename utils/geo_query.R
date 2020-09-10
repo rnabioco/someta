db10072257695f0c0d22865bdbfb5f99dad41630
raysinensis <raysinensis@gmail.com>
2020-09-09 20:29:08 -0600

    parsing geo query download result

--- utils/geo_query.R
+++ utils/geo_query.R
@@ -1,48 +1,53 @@
 library(tidyverse)
 library(here)
+library(GEOquery)
 proj_dir <- here()
 # this file of exported geo search results
 # "expression profiling by high throughput sequencing"[DataSet Type] AND "single cell"[All Fields] 
-fileloc <- file.path(proj_dir, "data", "geo_query", "gds_result_allsc.txt")
+fileloc <- file.path(proj_dir, "inst", "extdata", paste0("gds_result_", date, ".txt"))
 
 gds <- read_table(fileloc, col_names = FALSE) %>% 
+  mutate(num = as.numeric(str_remove(str_extract(X1, "^[0-9]+\\."), "\\."))) %>% 
+  fill(num, .direction = "down") %>% 
   filter(str_detect(X1, "Organism:|FTP download:")) %>% 
-  mutate(entry = floor((row_number() - 1) / 2), part = (row_number() - 1) %% 2) %>% 
-  pivot_wider(names_from = part, values_from = X1) %>% 
-  mutate(id = str_remove(str_remove(`1`, ".+nnn/"), "/")) %>% 
-  mutate(org = str_remove(`0`, "Organism:	")) %>% 
-  mutate(files = str_remove(str_remove(`1`, "^FTP download: GEO \\("), "\\)..+")) %>% 
-  filter(!str_detect(files, "FTP download: GEO")) %>% 
+  mutate(type = ifelse(str_detect(X1, "Organism:"), "org", "files")) %>% 
+  pivot_wider(names_from = type, values_from = X1) %>% 
+  mutate(id = str_remove(str_remove(files, ".+nnn/"), "/")) %>% 
+  mutate(org = str_remove(org, "Organism:	")) %>% 
+  mutate(files = str_remove(str_remove(files, "^FTP download: GEO \\("), "\\)..+")) %>% 
+  mutate(files = ifelse(str_detect(files, "FTP download: GEO"), "none", files)) %>% 
   select(id, org, files)
 
 geo_string <- function(id, string) {
   temp <- tryCatch(suppressMessages(getGEOSuppFiles(id, 
                                                     makeDirectory = F, 
                                                     fetch_files = F)),
-                   error = function(e) {"error1"})
+                   error = function(e) {"error_get"})
   if (is.null(temp)) {
-    return(FALSE)
+    return("no")
   } else {
     tryCatch(
-      temp$fname %>% 
-        str_to_lower() %>%
-        str_detect(string) %>% 
-        any(),
-      error = function(e) {"error2"})
+      ifelse(
+        temp$fname %>% 
+          str_to_lower() %>%
+          str_detect(string) %>% 
+          any(),
+        "yes",
+        "no"),
+      error = function(e) {"error_parse"})
   }
 }
 
-gds_h <- gds %>% filter(str_detect(org, "Homo sapiens")) %>%
-  mutate(has_meta = pbmcapply::pbmcmapply(geo_string, id, "meta"))
-gds_h <- gds_h %>% mutate(has_r = str_detect(files, "RDA|RDATA|RDS")) %>% 
-  mutate(usable = ifelse(has_meta | has_r, TRUE, FALSE))
+t0 <- Sys.time()
+gds <- gds %>%
+  mutate(has_meta = pbmcapply::pbmcmapply(geo_string, id, "meta")) %>%
+  filter(!str_detect(has_meta, "error")) %>%
+  mutate(has_r = ifelse(str_detect(files, "RDA|RDATA|RDS"), "yes", "no")) %>% 
+  mutate(usable = ifelse(has_meta == "yes" | has_r == "yes", "yes", "no"))
+message("GEOquery data check step took ", format(Sys.time() - t0))
 
-gds_m <- gds %>% filter(str_detect(org, "Mus musculus")) %>%
-  mutate(has_meta = pbmcapply::pbmcmapply(geo_string, id, "meta"))
-gds_m <- gds_m %>% filter(!str_detect(has_meta, "error")) %>%
-  mutate(has_meta = as.logical(has_meta)) %>% 
-  mutate(has_r = str_detect(files, "RDA|RDATA|RDS")) %>% 
-  mutate(usable = ifelse(has_meta | has_r, TRUE, FALSE))
+gds_h <- gds %>% filter(str_detect(org, "Homo sapiens"))
+gds_m <- gds %>% filter(str_detect(org, "Mus musculus"))
 
-write_tsv(gds_h, "geo_hs.tsv")
-write_tsv(gds_m, "geo_mm.tsv")
+write_tsv(gds_h, here("inst", "extdata", paste0("geo_hs_", date, ".tsv")))
+write_tsv(gds_m, here("inst", "extdata", paste0("geo_mm_", date, ".tsv")))
