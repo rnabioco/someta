f648b1a2b10475307150aea78aca93ecc9ec342b
raysinensis <raysinensis@gmail.com>
2020-11-17 15:55:56 -0700

    styler

--- utils/geo_query.R
+++ utils/geo_query.R
@@ -26,39 +26,55 @@ gds <- read_table(fileloc, col_names = FALSE) %>%
 # functions
 geo_supp <- function(id, string) {
   tryCatch(suppressMessages(getGEOSuppFiles(id,
-                                            makeDirectory = F,
-                                            fetch_files = F)),
-                   error = function(e) {"error_get"})
+    makeDirectory = F,
+    fetch_files = F
+  )),
+  error = function(e) {
+    "error_get"
+  }
+  )
 }
 
 supp_files <- function(temp) {
   tryCatch(
     temp$fname %>%
       str_to_lower(),
-    error = function(e) {"none"})
+    error = function(e) {
+      "none"
+    }
+  )
 }
 
 tar_files <- function(link) {
   tryCatch(
     read_tsv(link, comment = "#") %>% pull(2) %>%
       str_to_lower(),
-    error = function(e) {"error_parse"})
+    error = function(e) {
+      "error_parse"
+    }
+  )
 }
 
 check_string <- function(files, string) {
   ifelse(str_detect(files, string) %>% any(),
-         "yes",
-         "no")
+    "yes",
+    "no"
+  )
 }
 
 get_geo <- function(geo) {
   res <- tryCatch(
-    suppressMessages(GEOquery::getGEO(GEO = geo,
-                                      filename = NULL,
-                                      GSElimits = NULL, GSEMatrix = FALSE,
-                                      AnnotGPL = FALSE, getGPL = FALSE,
-                                      parseCharacteristics = FALSE)),
-    error = function(e) {"notavail"})
+    suppressMessages(GEOquery::getGEO(
+      GEO = geo,
+      filename = NULL,
+      GSElimits = NULL, GSEMatrix = FALSE,
+      AnnotGPL = FALSE, getGPL = FALSE,
+      parseCharacteristics = FALSE
+    )),
+    error = function(e) {
+      "notavail"
+    }
+  )
   if (class(res) != "GSE") {
     return(NA)
   }
@@ -71,22 +87,25 @@ get_date <- function(res) {
   } else {
     NA
   }
-
 }
 
 get_pubmed <- function(res) {
-  #res <- res[[1]]
+  # res <- res[[1]]
   if (class(res) != "list") {
     return(NA)
   }
 
   if (length(res$pubmed_id) > 1) {
     id2 <- tryCatch(res$pubmed_id[1],
-                    error = function(e) {"notfound"}
+      error = function(e) {
+        "notfound"
+      }
     )
   } else {
     id2 <- tryCatch(res$pubmed_id,
-                    error = function(e) {"notfound"}
+      error = function(e) {
+        "notfound"
+      }
     )
   }
 
@@ -107,7 +126,10 @@ get_pubmed <- function(res) {
 
 from_list <- function(pubmedid) {
   tryCatch(pubmeds[[pubmedid]],
-           error = function(e) {"error"})
+    error = function(e) {
+      "error"
+    }
+  )
 }
 
 get_journal <- function(pubmed) {
@@ -115,27 +137,41 @@ get_journal <- function(pubmed) {
     pubmed %>%
       dplyr::slice(1) %>%
       pull(jabbrv),
-    error = function(e) "error")
+    error = function(e) "error"
+  )
 }
 
 # step-by-step processing
 message("parse GEO supplementary files")
-gds <- gds %>% mutate(supp = pbmcapply::pbmcmapply(geo_supp,
-                                                   id))
-gds2 <- gds %>% mutate(suppfiles = pbmcapply::pbmcmapply(supp_files,
-                                                         supp))
-gds3 <- gds2 %>% mutate(tarfiles = pbmcapply::pbmcmapply(tar_files,
-                                                         link))
-gds4 <- gds3 %>% mutate(has_meta1 = pbmcapply::pbmcmapply(check_string,
-                                                          suppfiles,"meta|annot|type|clustering"))
-gds4 <- gds4 %>% mutate(has_meta2 = pbmcapply::pbmcmapply(check_string,
-                                                          tarfiles,"meta|annot|type|clustering"))
+gds <- gds %>% mutate(supp = pbmcapply::pbmcmapply(
+  geo_supp,
+  id
+))
+gds2 <- gds %>% mutate(suppfiles = pbmcapply::pbmcmapply(
+  supp_files,
+  supp
+))
+gds3 <- gds2 %>% mutate(tarfiles = pbmcapply::pbmcmapply(
+  tar_files,
+  link
+))
+gds4 <- gds3 %>% mutate(has_meta1 = pbmcapply::pbmcmapply(
+  check_string,
+  suppfiles, "meta|annot|type|clustering"
+))
+gds4 <- gds4 %>% mutate(has_meta2 = pbmcapply::pbmcmapply(
+  check_string,
+  tarfiles, "meta|annot|type|clustering"
+))
 gds4 <- gds4 %>% mutate(has_object1 = ifelse(str_detect(suppfiles, "rds|rda|rdata|loom|h5ad"),
-                                            "yes", "no"))
+  "yes", "no"
+))
 gds4 <- gds4 %>% mutate(has_object2 = ifelse(str_detect(tarfiles, "rds|rda|rdata|loom|h5ad"),
-                                            "yes", "no"))
+  "yes", "no"
+))
 gds4 <- gds4 %>% mutate(usable = ifelse(has_meta1 == "yes" | has_meta2 == "yes" | has_object1 == "yes" | has_object2 == "yes",
-                                        "yes", "no"))
+  "yes", "no"
+))
 
 # giant superseries (also incorrect) soft.gz  files cause issues
 blacklist <- c("GSE63058", "GSE47917")
@@ -143,22 +179,27 @@ gds4 <- gds4 %>% filter(!(id %in% blacklist))
 
 # geo and pubmed parsing, slow
 message("get GEO data")
-gds5 <- gds4 %>% split(gds4$id) %>% map_dfr(. %>% mutate(geo = map(id, get_geo)))
+gds5 <- gds4 %>%
+  split(gds4$id) %>%
+  map_dfr(. %>% mutate(geo = map(id, get_geo)))
 saveRDS(gds5, here("inst", "extdata", date, paste0("gds5_temp_", date, ".rds")))
 
 message("get PubMed data")
 gds5 <- readRDS(here("inst", "extdata", date, paste0("gds5_temp_", date, ".rds")))
-gds5 <- gds5 %>% mutate(date = pbmcapply::pbmcmapply(get_date, geo)) %>%
+gds5 <- gds5 %>%
+  mutate(date = pbmcapply::pbmcmapply(get_date, geo)) %>%
   separate(date, into = c("month", "day", "year"))
 gds5 <- gds5 %>% mutate(pubmed = pbmcapply::pbmcmapply(get_pubmed, geo))
 
 pubmeds <- list()
-splits <- split(gds5$pubmed %>% na.omit(), ceiling(seq_along(gds5$pubmed %>% na.omit())/20))
+splits <- split(gds5$pubmed %>% na.omit(), ceiling(seq_along(gds5$pubmed %>% na.omit()) / 20))
 for (s in splits) {
   query_string <- str_c(s, collapse = ",")
   pubmed_file <- get_pubmed_ids(query_string,
-                                api_key = "9cc836a53f5c509a6783283e694da4fb0d08")
-  pubmed <- pubmed_file %>% fetch_pubmed_data(retmax = 9999) %>%
+    api_key = "9cc836a53f5c509a6783283e694da4fb0d08"
+  )
+  pubmed <- pubmed_file %>%
+    fetch_pubmed_data(retmax = 9999) %>%
     articles_to_list()
   pubmeds1 <- sapply(pubmed, article_to_df, simplify = FALSE)
   names(pubmeds1) <- pubmed_file$IdList
@@ -166,17 +207,20 @@ for (s in splits) {
 }
 
 gds6 <- gds5 %>% mutate(pubmed = map(pubmed, from_list))
-gds6 <- gds6 %>% mutate(journal = pbmcapply::pbmcmapply(get_journal,
-                                                        pubmed))
+gds6 <- gds6 %>% mutate(journal = pbmcapply::pbmcmapply(
+  get_journal,
+  pubmed
+))
 
 get_cites <- function(pubmed) {
   tryCatch(
     cr_citation_count(pubmed$doi[1])$count,
-    error = function(e) "NA")
+    error = function(e) "NA"
+  )
 }
 message("get citation data")
-gds7 <- gds6 %>% mutate(cite = map(pubmed, get_cites)) %>%
+gds7 <- gds6 %>%
+  mutate(cite = map(pubmed, get_cites)) %>%
   mutate(year = as.numeric(year))
 gds7 %>% saveRDS(here("inst", "extdata", date, paste0("geo_", date, ".rds")))
 gds7 %>% saveRDS(here("inst", "extdata", "current_geo.rds"))
-
